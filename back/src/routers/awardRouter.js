6d3b03c427baf0cacd1bd338bf51e16c48394f14
hj <bearn47@gmail.com>
2022-03-24 11:56:42 +0900

    라우터

--- back/src/routers/awardRouter.js
+++ back/src/routers/awardRouter.js
@@ -1,102 +1,76 @@
 import is from "@sindresorhus/is";
 import { Router } from "express";
-import { awardService } from "../services/awardService";
 import { login_required } from "../middlewares/login_required";
-ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹ
-// req.body 형식 
-// {
-//     "user_id":"af4ff0af-2a5f-4eea-99f2-d18b42aba419",
-//     "title":"운전면허증",
-//     "description":"2종 보통입니다.",
-//     "when_date":"2021-03-20"
-// }
+import { AwardService } from "../services/awardService";
 
-//test user id :184fb386-57a2-4a8d-bb6e-4241298e9455
-// test id: 461189c3-2ee4-4aa7-b7b3-9ec4827931e0
+const awardRouter = Router();
 
-const awardRouter = Router()
+awardRouter.post("/award/create", async function (req, res, next) {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
+    }
 
-awardRouter.post('/award/create', async function (req, res, next) {
-    try{
-        if (is.emptyObject(req.body)){
-            throw new Error ("headers의 Content-Type을 application/json으로 설정해주세요")
-        }
+    // req (request) 에서 데이터 가져오기
+    // console.log(req.body);
+    const user_id = req.body.user_id;
+    const title = req.body.title;
+    const description = req.body.description;
+    
 
-        // req 에서 데이터 가져오기
-        const {user_id, title, description} = req.body
+    // 위 데이터를 유저 db에 추가하기
+    //Cannot read properties of undefined (reading 'addProject') 에러발생
+    const newAward = await AwardService.addAward({
+      user_id,
+      title,
+      description,
+      
+    });
+    if (newAward.errorMessage) {
+      throw new Error(newAward.errorMessage);
+    }
 
-        // 위 데이터를 certificate db에 추가
-        const newAward = await awardService.addAward({
-            user_id,
-            title,
-            description,
-            
-        })
+    res.status(201).json(newAward);
+  } catch (error) {
+    next(error);
+  }
+});
 
-        if (newAward.errorMessage) {
-            throw new Error(newAward.errorMessage)
-        }
+awardRouter.get(
+  "/awardlist/:id",
+  login_required,
+  async function (req, res, next) {
+    try {
+      // URI로부터 사용자 id를 추출함.
+      console.log(req.params);
+      const user_id = req.params.id;
+      console.log(user_id);
 
-        res.status(201).json(newAward)
+      const award_list = await AwardService.getAwardList({ user_id });
+      res.status(200).send(award_list);
     } catch (error) {
-        next(error)
+      next(error);
     }
-})
+  }
+);
 
-awardRouter.get('/awards/:id', login_required, async function (req, res, next) {
-    try{
-      // URL 로 부터 id를 추출
-        const award_id = req.params.id
-        const award = await awardService.getAward({award_id})  
-        
-        if (award.errorMessage) {
-            throw new Error(award.errorMessage)
-        }
-        res.status(200).send(award)
+awardRouter.get(
+  "/awards/:id",
+  login_required,
+  async function (req, res, next) {
+    try {
+      const awards_id = req.params.id;
+      // console.log(projects_id);
+
+      const awards = await AwardService.getAwards({ awards_id });
+      res.status(200).send(awards);
     } catch (error) {
-        next(error)
+      next(error);
     }
-})
+  }
+);
 
-awardRouter.put('/awards/:id', login_required, async function (req, res, next) {
-    try{
-        const award_id = req.params.id
 
-        const {title, description} = req.body
-        const toUpdate = {
-            title,
-            description,
-        
-        }
-     
-        const updatedAward = await awardService.setAward({ award_id, toUpdate})
-
-        if (updatedAward.errorMessage){
-            throw new Error(updatedAward.errorMessage)
-        }
- 
-        res.status(200).json(updatedAward)
-
-     } catch (error) {
-        next(error)
-    }
-
-})
-
-awardRouter.get('/awardlist/:user_id', login_required, async function (req, res, next) {
-    try{
-        // URL 로부터 user_id 추출 
-        const user_id = req.params.user_id
-        const certis = await certificateService.getCertis({user_id})
-
-        if (certis.errorMessage){
-            throw new Error(certis.errorMessage)
-        }
-
-        res.status(200).send(certis)
-    } catch (error) {
-        next(error)
-    }
-})
-
-export { certificateRouter }
+export { awardRouter };
